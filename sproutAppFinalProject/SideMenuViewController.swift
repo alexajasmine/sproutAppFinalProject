7dc304e62a519c822996c8b2dd009f5be071a49f
Alexa Der <alexajasmine.der333@gmail.com>
2021-07-01 21:03:29 -0700

    fixed button

--- sproutAppFinalProject/SideMenuViewController.swift
+++ sproutAppFinalProject/SideMenuViewController.swift
@@ -15,29 +15,29 @@ class SideMenuViewController: UIViewController, UITableViewDelegate {
    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // TableView
-               self.sideMenuTableView.delegate = self
-               self.sideMenuTableView.dataSource = self
-               self.sideMenuTableView.backgroundColor = #colorLiteral(red: 0.737254902, green: 0.1294117647, blue: 0.2941176471, alpha: 1)
-               self.sideMenuTableView.separatorStyle = .none
-
-               // Set Highlighted Cell
-               DispatchQueue.main.async {
-                   let defaultRow = IndexPath(row: self.defaultHighlightedCell, section: 0)
-                   self.sideMenuTableView.selectRow(at: defaultRow, animated: false, scrollPosition: .none)
-               }
-
-//               // Footer
-//               self.footerLabel.textColor = UIColor.white
-//               self.footerLabel.font = UIFont.systemFont(ofSize: 12, weight: .bold)
-//               self.footerLabel.text = "Developed by John Codeos"
-
-               // Register TableView Cell
-               self.sideMenuTableView.register(sideMenuTableView.nib, forCellReuseIdentifier: sideMenuTableView.identifier)
-
-               // Update TableView with the data
-               self.sideMenuTableView.reloadData()
-     
+//        // TableView
+//               self.sideMenuTableView.delegate = self
+//               self.sideMenuTableView.dataSource = self
+//               self.sideMenuTableView.backgroundColor = #colorLiteral(red: 0.737254902, green: 0.1294117647, blue: 0.2941176471, alpha: 1)
+//               self.sideMenuTableView.separatorStyle = .none
+//
+//               // Set Highlighted Cell
+//               DispatchQueue.main.async {
+//                   let defaultRow = IndexPath(row: self.defaultHighlightedCell, section: 0)
+//                   self.sideMenuTableView.selectRow(at: defaultRow, animated: false, scrollPosition: .none)
+//               }
+//
+////               // Footer
+////               self.footerLabel.textColor = UIColor.white
+////               self.footerLabel.font = UIFont.systemFont(ofSize: 12, weight: .bold)
+////               self.footerLabel.text = "Developed by John Codeos"
+//
+//               // Register TableView Cell
+//               self.sideMenuTableView.register(sideMenuTableView.nib, forCellReuseIdentifier: sideMenuTableView.identifier)
+//
+//               // Update TableView with the data
+//               self.sideMenuTableView.reloadData()
+//
         
 
         // Do any additional setup after loading the view.
