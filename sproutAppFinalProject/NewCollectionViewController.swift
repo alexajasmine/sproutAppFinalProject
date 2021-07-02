001c3dad202a32b5a29c12aa91a6ff4ca6767d89
akatanate <rinmunakata@gmail.com>
2021-07-02 11:14:18 -0700

    fixed to-do list

--- sproutAppFinalProject/NewCollectionViewController.swift
+++ sproutAppFinalProject/NewCollectionViewController.swift
@@ -7,6 +7,38 @@
 
 import UIKit
 var selectedArray : [Journal] = []
+//let darkGreen = UIColor(named : "4A5759")
+//let lightGreen = UIColor(named : "B0C4B1")
+
+//extension UIColor {
+//    public convenience init?(hex: String) {
+//        let r, g, b, a: CGFloat
+//
+//        if hex.hasPrefix("#") {
+//            let start = hex.index(hex.startIndex, offsetBy: 1)
+//            let hexColor = String(hex[start...])
+//
+//            if hexColor.count == 8 {
+//                let scanner = Scanner(string: hexColor)
+//                var hexNumber: UInt64 = 0
+//
+//                if scanner.scanHexInt64(&hexNumber) {
+//                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+//                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+//                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+//                    a = CGFloat(hexNumber & 0x000000ff) / 255
+//
+//                    self.init(red: r, green: g, blue: b, alpha: a)
+//                    return
+//                }
+//            }
+//        }
+//
+//        return nil
+    
+
+
+
 
 class NewCollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
@@ -53,15 +85,15 @@ class NewCollectionViewController: UIViewController, UICollectionViewDataSource,
     // change background color when user touches cell
     func collectionView(_ collectionView: UICollectionView, didHighlightItemAt indexPath: IndexPath) {
         let cell = collectionView.cellForItem(at: indexPath)
-        cell?.backgroundColor = UIColor.red
+        cell?.backgroundColor = UIColor.white
     }
 
     // change background color back when user releases touch
     func collectionView(_ collectionView: UICollectionView, didUnhighlightItemAt indexPath: IndexPath) {
         let cell = collectionView.cellForItem(at: indexPath)
-        cell?.backgroundColor = UIColor.red
-    }
+        cell?.backgroundColor = UIColor.white    }
     
    }
+
 
 
